05bd83c218e8647656861d40164bc3bc9d0f516e
Arbs28 <plakuarber1@gmail.com>
2025-05-03 19:50:06 +0200

    UI changes

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
-const Layout = ({ children, isLoggedIn, onLogout, user }) => {
+const Layout = ({ children, isLoggedIn, onLogout, user, toggleDarkMode }) => {
   return (
     <Box
       sx={{
@@ -11,7 +11,7 @@ const Layout = ({ children, isLoggedIn, onLogout, user }) => {
         minHeight: '100vh',
       }}
     >
-      <Navbar isLoggedIn={isLoggedIn} onLogout={onLogout} user={user} />
+      <Navbar isLoggedIn={isLoggedIn} onLogout={onLogout} user={user} toggleDarkMode={toggleDarkMode} />
       <Box sx={{ flexGrow: 1 }}>
         {children}
       </Box>
